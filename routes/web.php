42e96beafc2b7684ca5aef160de3430b23568e3e
yukiforwork <yukiforwork@gmail.com>
2022-02-12 06:18:16 +0000

    user register and login

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,16 @@
 
 // デフォルトのコメント部分は省略
 
+// ユーザ登録
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
+// 認証
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
+
+// タスク
 Route::get('/', 'TasksController@index');
 
 Route::resource('tasks', 'TasksController');
